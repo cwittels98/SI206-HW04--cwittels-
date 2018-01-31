9b06ca5ecdc3e7ff2973434e87d27e42be128735
Charlotte Wittels <cwittels98@gmail.com>
2018-01-31 18:20:54 -0500

    added checking

--- magic_eight.py
+++ magic_eight.py
@@ -1,1 +1,9 @@
 user_input = input("What is your question?")
+
+while True:
+    if [-1] != ?
+
+    user_input = ("I'm sorry, I can only answer questions. Please ask a question, or write "exit" to quit")
+
+if user_input.lower() == "exit":
+  break
