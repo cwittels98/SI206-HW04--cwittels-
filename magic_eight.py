4876458cb0aec178e46067eb6f83502df121d423
cwittels98 <35805762+cwittels98@users.noreply.github.com>
2018-02-01 18:10:23 -0500

    Update magic_eight.py

--- magic_eight.py
+++ magic_eight.py
@@ -1,13 +1,17 @@
-user_input = input("What is your question?")
-while True:
-    if [-1] != ?
+import random
 
-    user_input = ("I'm sorry, I can only answer questions. Please ask a question, or write "exit" to quit")
-
-if user_input.lower() == "exit":
-  break
-lst = ["It is certain", "It is decidedly so, Without a doubt",
-"Yes definitely", "You may rely on it", "As I see it", "yes", "Most likely", "Outlook good", "Yes",
-"Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now"
-"Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
-answer=random.randrange(len(lst))
+def ask_question():
+    user_input = ""
+    while user_input != exit:
+        user_input = input("What is your question?")
+        if user_input == "exit":
+            break
+        if user_input[-1] != "?":
+            print ("Sorry, I can only answer qustions. Please ask another question, or write "exit" to quit.")
+        else:
+            lst_of_resposes = ["It is certain", "It is decidedly so, Without a doubt",
+                               "Yes definitely", "You may rely on it", "As I see it", "yes", "Most likely", "Outlook good", "Yes",
+                               "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now"
+                               "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
+            answer = random.choice(lst_of_responses)
+            print answer
