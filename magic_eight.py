efc05007d07bc84cca55ce5e5c0f15986b5a29f8
cwittels98 <35805762+cwittels98@users.noreply.github.com>
2018-03-11 22:15:08 -0400

    updated_magic_for_credit

--- magic_eight.py
+++ magic_eight.py
@@ -1,17 +1,21 @@
 import random
 
-def ask_question():
+def ask_question(user_input):
     user_input = ""
     while user_input != exit:
-        user_input = input("What is your question?")
+        user_input = input("What is your question? ")
         if user_input == "exit":
             break
         if user_input[-1] != "?":
-            print ("Sorry, I can only answer qustions. Please ask another question, or write "exit" to quit.")
+            print ("Sorry, I can only answer qustions. Please ask another question, or write 'exit' to quit.")
         else:
-            lst_of_resposes = ["It is certain", "It is decidedly so, Without a doubt",
+            lst_of_responses = ["It is certain", "It is decidedly so, Without a doubt",
                                "Yes definitely", "You may rely on it", "As I see it", "yes", "Most likely", "Outlook good", "Yes",
                                "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now"
                                "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
+
             answer = random.choice(lst_of_responses)
-            print answer
+            print (answer)
+
+
+user_input = ask_question("What is your question?")
